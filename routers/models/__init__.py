0eec671c36365dd6caf11fa875541f9c2009e0db
fkai54570-eng <fkai54570@gmail.com>
2026-04-05 00:45:39 +0800

    Create __init__.py

---
+++ routers/models/__init__.py
@@ -0,0 +1,19 @@
+from .schemas import (
+    ScrapeRequest,
+    ExtractRequest,
+    ScrapeResponse,
+    ErrorResponse,
+    HealthResponse,
+    Metadata,
+    APIKeyInfo,
+)
+
+__all__ = [
+    "ScrapeRequest",
+    "ExtractRequest",
+    "ScrapeResponse",
+    "ErrorResponse",
+    "HealthResponse",
+    "Metadata",
+    "APIKeyInfo",
+]
